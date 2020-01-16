8183ab84b23c357ab264472b9038b4c35ce10d75
Robert Clark <robert.d.clark@hpe.com>
2020-01-16 15:24:23 -0600

    Update

--- gym_mupen64plus/envs/MarioTennis/mario_tennis_env.py
+++ gym_mupen64plus/envs/MarioTennis/mario_tennis_env.py
@@ -1,5 +1,6 @@
 import inspect
 import yaml
+from abc import ABCMeta
 from gym import spaces
 from gym_mupen64plus.envs.mupen64plus_env import (ControllerState,
                                                   Mupen64PlusEnv)
@@ -7,6 +8,8 @@ from os.path import dirname, join
 
 
 class MarioTennisEnv(Mupen64PlusEnv):
+    __metaclass__ = ABCMeta
+
     def __init__(self, player='mario', opponent='luigi', court='open'):
         self._set_players(player, opponent)
 
@@ -20,7 +23,16 @@ class MarioTennisEnv(Mupen64PlusEnv):
                                                   [   0,   1],
                                                   [   0,   1]])
 
+    def _step(self, action):
+        # Append unneeded inputs
+        num_missing = len(ControllerState.A_BUTTON) - len(action)
+        full_action = action + [0] * num_missing
+        return super(MarioTennisEnv, self)._step(full_action)
+
     def _reset(self):
+        if self.reset_count > 0:
+            with self.controller_server.frame_skip_disabled():
+                pass
         return super(MarioTennisEnv, self)._reset()
 
     def _get_reward(self):
@@ -35,31 +47,73 @@ class MarioTennisEnv(Mupen64PlusEnv):
         self.config.update(config)
 
     def _validate_config(self):
-        return True
+        print('validate config')
+        gfx_plugin = self.config['GFX_PLUGIN']
 
     def _navigate_menu(self):
         self._navigate_start_menu()
         self._navigate_player_select()
+        self._navigate_game()
+        self._select_opponent()
+        self._select_court()
+        # Skip intro
+        self._wait(count=200, wait_for='Pregame intro')
+        self._act(ControllerState.A_BUTTON, count=2)
 
     def _navigate_start_menu(self):
-        self._wait(count=100, wait_for='Splash Screen')
-        self._press_button(ControllerState.START_BUTTON)
-        self._wait(count=30, wait_for='Main Menu')
+        self._wait(count=140, wait_for='Nintendo Screen')
+        self._act(ControllerState.A_BUTTON)
+
+        self._wait(count=230, wait_for='Intro Video')
+        self._act(ControllerState.A_BUTTON)
+
+        self._wait(count=190, wait_for='Splash Screen')
+        self._act(ControllerState.START_BUTTON)
+
         # Select single player
-        self._press_button(ControllerState.A_BUTTON)
+        self._wait(count=70, wait_for='Main Menu')
+        self._act(ControllerState.A_BUTTON, count=2)
 
     def _navigate_player_select(self):
-        self._wait(count=30, wait_for='Player Select')
-        self._player_select()
+        self._wait(count=60, wait_for='Player Select')
+        self._player_select(self._player_pos)
 
-    def _player_select(self):
-        row, col = self._player_pos
+    def _player_select(self, player_pos):
+        row, col = player_pos
         # Navigate to the requested player according to their pre-defined row
         # and column values.
         self._press_button(ControllerState.JOYSTICK_DOWN, times=row)
         self._press_button(ControllerState.JOYSTICK_RIGHT, times=col)
         # Select the requested player
-        self._press_button(ControllerState.A_BUTTON)
+        self._act(ControllerState.A_BUTTON, count=2)
+
+    def _navigate_game(self):
+        # Select Exhibition mode
+        self._wait(count=100, wait_for='Game Selection')
+        self._act(ControllerState.A_BUTTON, count=2)
+
+        # Select singles
+        self._wait(count=50, wait_for='Number of Players')
+        self._act(ControllerState.A_BUTTON, count=2)
+
+        # Select 1 set with 2 games
+        self._wait(count=50, wait_for='Match duration')
+        self._act(ControllerState.A_BUTTON, count=2)
+
+    def _select_opponent(self):
+        # Select computer opponent
+        self._wait(count=50, wait_for='Opponent select')
+        #self._player_select(self._opponent_pos)
+        self._act(ControllerState.A_BUTTON, count=2)
+
+        # Select difficulty
+        self._wait(count=10, wait_for='Difficulty select')
+        self._act(ControllerState.JOYSTICK_LEFT)
+        self._act(ControllerState.A_BUTTON, count=2)
+
+    def _select_court(self):
+        self._wait(count=150, wait_for='Court selection')
+        self._act(ControllerState.A_BUTTON, count=2)
 
     def _set_players(self, player, opponent):
         players = {
